e0e819e310d9b281c13a9e5d4e22a92218b5eda4
Diiego2 <diego.ara@sidgt.com>
2015-03-25 08:20:07 -0600

    Revert "codigo"
    This reverts commit a1f773997e38a976cf42e49869c0086823fd06a3.

--- javascript/exp.js
+++
@@ -1,5 +0,0 @@
-function mostraraletra() { alert('hizo clic!'); }
-
-function hacerclic() { document.getElementsByTagName('input')[0].onclick = mostraraletra; }
-
-window.onload = hacerclic;
--- javascript/extension.js
+++
@@ -1,5 +0,0 @@
-function mostraraletra() { alert('hizo clic!'); }
-
-function hacerclic() { document.getElementsByTagName('input')[2].onclick = mostraraletra; }
-
-window.onload = hacerclic;
